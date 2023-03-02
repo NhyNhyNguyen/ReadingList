a1d963be827a9462fbdbd6023d034fdda2005499
zenny <dddnhi@gmail.com>
2023-03-02 23:54:38 +0700

    init project

---
+++ src/main/java/com/example/readinglist/Book.java
@@ -0,0 +1,33 @@
+package com.example.readinglist;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String reader;
+    private String isbn;
+    private String title;
+    private String author;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReader() {
+        return reader;
+    }
+
+    public void setReader(String reader) {
+        this.reader = reader;
+    }
+}
